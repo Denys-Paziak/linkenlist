aa108810974e5e7b042901a8db2572dd0be6a276
En.Theos <theosenigma@gmail.com>
2025-12-02 17:39:09 +0200

    fix front config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,20 +2,16 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: false,
     remotePatterns: [
       { protocol: 'https', hostname: 'd3lehtoxndxwf6.cloudfront.net', pathname: '/**' }
     ],
   },
-  async rewrites() {
-    return [
-      { source: '/backend/:path*', destination: 'http://localhost:3001/:path*' },
-    ]
-  }
-}
+};
 
-export default nextConfig
+export default nextConfig;
